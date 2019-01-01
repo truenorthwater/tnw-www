54cad38d5320d3dde9cda46ddf42c08ca7262c84
Jesse Kernaghan <jesse@versett.com>
2019-01-01 11:04:18 -0800

    tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: '',
+        trackingId: 'UA-131541335-1',
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
